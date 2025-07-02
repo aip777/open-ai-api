7fdc3984e4d1c6875d7791bbf9b1503d0ad44e4a
Ariful Islam Palash <ariful.islamk777@gmail.com>
2025-07-02 16:48:07 +0600

    Open AI Blog

--- open_ai_blog.py
+++ open_ai_blog.py
@@ -1,20 +1,30 @@
+import pandas as pd
 from openai import OpenAI
 import redis
 import json
 from decouple import config
+
 
 SECRET_KEY = config('SECRET_API_KEY')
 client = OpenAI(api_key=SECRET_KEY)
 
 r = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
 
-def generate_response(user_id, user_message, tone="Neutral", audience="General", format="Article", word_count=800):
-    key = f"user:{user_id}:chat"
+def generate_response(user_id, blog_number, user_message, tone="Neutral", audience="General", format="Article", word_count=800):
+    key = f"user:{user_id}:blog:{blog_number}"
+
     if r.exists(key):
         messages = json.loads(r.get(key))
+        messages.append({"role": "user", "content": str(user_message)})
     else:
         messages = [{"role": "system", "content": "You are a helpful blog writer."}]
-    messages.append({"role": "user", "content": str(user_message) })
+        prompt = f"""
+                Write a {format.lower()} blog post about "{user_message}".
+                Tone: {tone}.
+                Audience: {audience}.
+                Word count: Approximately {word_count} words.
+                """
+        messages.append({"role": "user", "content": str(prompt) })
     try:
         response = client.chat.completions.create(
             model="gpt-4o-mini",
@@ -34,6 +44,5 @@ def generate_response(user_id, user_message, tone="Neutral", audience="General",
             "error": str(e)
         }
 
-result = generate_response(user_id="566", user_message='Can you suggest me some restaurants with location', tone="Informative", format="Travel Blog")
-# print(result["blog"] if result["success"] else result["error"])
+result = generate_response(user_id="332", blog_number='449', user_message='Life style', tone="Informative", format="Travel")
 print(result)
